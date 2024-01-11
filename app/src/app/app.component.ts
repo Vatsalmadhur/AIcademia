401d7615d1d74df2ee71f4d7ec80dc09c596932c
Madhur Bharti <madhur@Madhurs-MacBook-Pro.local>
2024-01-11 22:02:29 +0530

    Navbar

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@NgModule ({
+  imports: [
+    MatSlideToggleModule,
+  ]
+})
+export class AppModule {}
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet,NavbarComponent,RouterModule],
   templateUrl: './app.component.html',
-  // styleUrl: './navbar/navbar.component.sass'
+  styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'app';
--- app/src/main.ts
+++ app/src/main.ts
@@ -4,11 +4,13 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import routeConfig from './app/routes';
 import { provideProtractorTestingSupport } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent,   {
   providers: [
     provideProtractorTestingSupport(),
-    provideRouter(routeConfig)
-  ]
+    provideRouter(routeConfig),
+    provideAnimations()
+]
 })
   .catch((err) => console.error(err));
