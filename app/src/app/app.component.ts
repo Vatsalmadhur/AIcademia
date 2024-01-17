7844327d6ee37704ef3bd6231456e5db73c98f93
Madhur Bharti <madhur@Madhurs-MacBook-Pro.local>
2024-01-17 20:17:22 +0530

    Almost Complete

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { DwButtonComponent } from './dw-button/dw-button.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,NavbarComponent,RouterModule,DwButtonComponent],
+  imports: [CommonModule, RouterOutlet,NavbarComponent,RouterModule,DwButtonComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
